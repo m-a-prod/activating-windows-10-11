cefce2b0732812a92d1b9b08d541a1d750f80187
Максим Адаев <maximkad2008@gmail.com>
2022-03-11 13:16:28 +0300

    fixed some stupid bugs

--- activation.cmd
+++ activation.cmd
@@ -5,6 +5,7 @@ title Activate
 
 :start
 echo Choose os
+echo.
 echo 1.Win 10/11 Pro
 echo 2.Win 10/11 Pro for workstations
 echo 3.Win 10/11 for educational institutions
@@ -12,11 +13,11 @@ echo 4.Win 10/11 Corporate
 echo.
 echo 0.Exit
 set /p var=Type some number : 
-if %var% ==1 goto pro
-if %var% ==2 goto workst
-if %var% ==3 goto educate
-if %var% ==4 goto corporate
-if %var% ==0 exit
+if %var% == 1 goto pro
+if %var% == 2 goto workst
+if %var% == 3 goto educate
+if %var% == 4 goto corporate
+if %var% == 0 exit
 goto start
 
 :pro
@@ -24,7 +25,7 @@ cls
 title pro
 echo Activate Win 10/11 Pro
 slmgr/ipk W269N-WFGWX-YVC9B-4J6C9-T83GX
-timeout /T -1 /NOBREAK>nul
+timeout /T 1 /NOBREAK>nul
 slmgr /skms kms.digiboy.ir
 goto exit
 
@@ -33,7 +34,7 @@ cls
 title Pro for workstations
 echo Activate Win 10/11 Pro
 slmgr/ipk NRG8B-VKK3Q-CXVCJ-9G2XF-6Q84J
-timeout /T -1 /NOBREAK>nul
+timeout /T 1 /NOBREAK>nul
 slmgr /skms kms.digiboy.ir
 goto exit
 
@@ -42,7 +43,7 @@ cls
 title for educational institutions 
 echo Activate Win 10/11 for educational institutions
 slmgr/ipk 6TP4R-GNPTD-KYYHQ-7B7DP-J447Y
-timeout /T -1 /NOBREAK>nul
+timeout /T 1 /NOBREAK>nul
 slmgr /skms kms.digiboy.ir
 goto exit
 
@@ -51,6 +52,6 @@ cls
 title for corporate
 echo Win 10/11 Corporate
 slmgr/ipk NPPR9-FWDCX-D2C8J-H872K-2YT43
-timeout /T -1 /NOBREAK>nul
+timeout /T 1 /NOBREAK>nul
 slmgr /skms kms.digiboy.ir
 goto exit
