a16fbf8c5d2b038de1d1747541b2fa5ee8ba8aec
Максим Адаев <maximkad2008@gmail.com>
2023-01-24 20:53:25 +0300

    Update activation.cmd

--- activation.cmd
+++ activation.cmd
@@ -11,16 +11,18 @@ echo.
 echo 1. Windown 10/11
 echo 2. Windows 8/8.1
 echo 3. Windows 7
-echo 4. Windows Server 2022
+echo 4. Windows Vista
+echo 5. Windows Server 2022
 echo.
-echo 5. Our GitHub
+echo 6. Our GitHub
 echo 0. Exit
 set /p var=Type some number : 
 if "%var%" == "1" call :win1011
-if "%var%" == "3" call :win7
 if "%var%" == "2" call :win8ch
-if "%var%" == "4" call :winserver2022
-if "%var%" == "5" start https://github.com/maxim-adaev-production/activating-windows-10-11
+if "%var%" == "3" call :win7
+if "%var%" == "5" call :winserver2022
+if "%var%" == "4" call :winVista
+if "%var%" == "6" start https://github.com/m-a-prod/activating-windows-10-11
 if "%var%" == "0" exit
 goto menu
 
@@ -122,6 +124,27 @@ if "%var%" == "1" call :activate FJ82H-XT6CR-J8D7P-XQJJ2-GPDD4
 if "%var%" == "2" call :activate 33PXH-7Y6KF-2VJC9-XBBR8-HVTHH
 if "%var%" == "0" goto :eof
 goto win7
+
+:winVista
+%r%
+cls
+title Windows Vista
+echo Choose your Windows Vista version
+echo.
+echo 1. Windows Vista Business
+echo 2. Windows Vista Business N
+echo 3. Windows Vista Enterprise
+echo 4. Windows Vista Enterprise N
+echo.
+echo 0. Exit
+set /p var=Type some number : 
+if "%var%" == "1" call :activate YFKBB-PQJJV-G996G-VWGXY-2V3X8
+if "%var%" == "2" call :activate HMBQG-8H2RH-C77VX-27R82-VMQBT
+if "%var%" == "3" call :activate VKK3X-68KWM-X2YGT-QR4M6-4BWMV
+if "%var%" == "4" call :activate VTC42-BM838-43QHV-84HX6-XJXKV
+if "%var%" == "0" goto :eof
+goto winVista
+
 
 :activate
 cls
